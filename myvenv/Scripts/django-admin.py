a368f95b9512c213373c1f9e59afcc5b371af6ba
Your Name <you@example.com>
2020-05-23 17:57:44 +0200

    first app

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\czere\desktop\placezabaw\sklep\moze2\django-ecommerce\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
